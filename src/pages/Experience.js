e333260b8007f4ca8fff1eed3da51d4d8e8e9de4
Marcos <marcoslopezsanchez@hotmail.com>
2022-07-02 00:05:30 -0500

    Inicio a modificar Experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { DiReact } from "react-icons/di";
+import { AiOutlineSolution } from "react-icons/ai"
 
 import "../styles/Experience.css";
 
@@ -6,7 +8,8 @@ const Experience = () => {
   return (
     <div className="service component__space" id="Services">
       <div className="heading">
-        <h1 className="heading">Experiencia</h1>
+        <h1 className="heading"> <DiReact />
+          Experiencia</h1>
       </div>
       <div className="container">
         <div div className="row">
@@ -54,59 +57,59 @@ const Experience = () => {
         </div>
       </div>
       <div className="heading">
-        <h1 className="heading">Formación Academica</h1>
+        <h1 className="heading"><AiOutlineSolution />Formación Academica</h1>
       </div>
       <div className="container">
         <div div className="row">
 
-        <div className="col__3">
-          <div className="service__box pointer">
-            <div className="service__meta">
-              <h1 className="service__text">
-                TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA
-              </h1>
-              <p className="compania service__text p__color">
-                Ingeniería en Sistemas Computacionales - Septiembre 2013 - Marzo
-                2018
-              </p>
-              <p className="p service__text p__color">
-                -Organización y Funcionamiento de toda la estructura
-                computacional.
-              </p>
-              <p className="p service__text p__color">
-                -Me instruí conociendo los componentes principales en software y
-                hardware.
-              </p>
-              <p className="p service__text p__color">
-                - Analizar situaciones con el fin de conocer y solucionar
-                complejidades en el sistema de cómputo.
-              </p>
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="service__meta">
+                <h1 className="service__text">
+                  TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA
+                </h1>
+                <p className="compania service__text p__color">
+                  Ingeniería en Sistemas Computacionales - Septiembre 2013 - Marzo
+                  2018
+                </p>
+                <p className="p service__text p__color">
+                  -Organización y Funcionamiento de toda la estructura
+                  computacional.
+                </p>
+                <p className="p service__text p__color">
+                  -Me instruí conociendo los componentes principales en software y
+                  hardware.
+                </p>
+                <p className="p service__text p__color">
+                  - Analizar situaciones con el fin de conocer y solucionar
+                  complejidades en el sistema de cómputo.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col__3">
-          <div className="service__box pointer">
-            <div className="service__meta">
-              <h1 className="service__text">
-                COLEGIO DE BACHILLERES DEL ESTADO DE OAXACA
-              </h1>
-              <p className="compania service__text p__color">
-                Administración de Empresas - Julio 2010 - Agosto 2013
-              </p>
-              <p className="p service__text p__color">
-                -Aprendí la organización, planeación, y análisis de situaciones
-                con el fin de beneficiar a la empresa.
-              </p>
-              <p className="p service__text p__color">
-                -Observación y Supervisión al planear alguna actividad.
-              </p>
-              <p className="p service__text p__color">
-                -Administración, dirección y gestión de empresas.
-              </p>
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="service__meta">
+                <h1 className="service__text">
+                  COLEGIO DE BACHILLERES DEL ESTADO DE OAXACA
+                </h1>
+                <p className="compania service__text p__color">
+                  Administración de Empresas - Julio 2010 - Agosto 2013
+                </p>
+                <p className="p service__text p__color">
+                  -Aprendí la organización, planeación, y análisis de situaciones
+                  con el fin de beneficiar a la empresa.
+                </p>
+                <p className="p service__text p__color">
+                  -Observación y Supervisión al planear alguna actividad.
+                </p>
+                <p className="p service__text p__color">
+                  -Administración, dirección y gestión de empresas.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
