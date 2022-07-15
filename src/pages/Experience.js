ccf1ede5dbbb24102ecbf98b25e8584fad44493a
Marcos <marcoslopezsanchez@hotmail.com>
2022-07-15 10:17:23 -0500

    Conectando a github

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,52 +13,100 @@ const Experience = () => {
       </div>
       <div className="container">
         <div div className="row">
-          <div className="col__3">
+          <div className="col__2">
             <div className="service__box pointer">
               <div className="service__meta">
-                <h1 className="service__text">Operador de Recepción Digital</h1>
-                <p className="compania service__text p__color">
-                  El UNIVERSAL COMPAÑÍA PERIODISTICA NACIONAL - Febrero 2020 -
-                  2021
-                </p>
-                <p className="p service__text p__color">
-                  -Consignar archivos en dirección a ctp’s. (computer to plate)
-                </p>
-                <p className="p service__text p__color">
-                  -Formación de Plantillas para páginas a rotativas.
-                </p>
-                <p className="p service__text p__color">
-                  -Coordinación, Planeación y ejecución del mantenimiento.
+                <p className="service__text service__date">
+                  - Febrero 2020 - 2021
                 </p>
               </div>
             </div>
           </div>
-
-          <div className="col__3">
+          <div className="col__2">
             <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">EMPAQUE</h1>
-                <p className="compania service__text p__color">
-                  El UNIVERSAL COMPAÑÍA PERIODISTICA NACIONAL - Febrero 2017 -
-                  2020
-                </p>
-                <p className="p service__text p__color">
-                  - Destinar pacas para su distribución.
-                </p>
-                <p className="p service__text p__color">
-                  -Flejar paquetes para asignar un destino.
-                </p>
-                <p className="p service__text p__color">
-                  -configurar denominadas rotativas Regioman y Harris.
-                </p>
-              </div>
+              <h1 className="service__text">
+                Operador de Recepción Digital
+              </h1>
+              <p className="p service__text p__color">
+                -Consignar archivos en dirección a ctp’s. (computer to plate)
+              </p>
+              <p className="p service__text p__color">
+                -Formación de Plantillas para páginas a rotativas.
+              </p>
+              <p className="p service__text p__color">
+                -Coordinación, Planeación y ejecución del mantenimiento.
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <div className="container">
+        <div div className="row">
+          <div className="col__2">
+            <div className="service__box pointer">
+              <div className="service__meta">
+                <p className="service__text service__date">
+                  - Febrero 2017 - 2020
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col__2">
+            <div className="service__box pointer">
+              <h1 className="service__text">
+                EMPAQUE
+              </h1>
+              <p className="p service__text p__color">
+                - Destinar pacas para su distribución.
+              </p>
+              <p className="p service__text p__color">
+                -Flejar paquetes para asignar un destino.
+              </p>
+              <p className="p service__text p__color">
+                -configurar denominadas rotativas Regioman y Harris.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="heading">
         <h1 className="heading"><AiOutlineSolution />Formación Academica</h1>
       </div>
+      <div className="container">
+        <div div className="row">
+          <div className="col__2">
+            <div className="service__box pointer">
+              <div className="service__meta">
+                <p className="service__text service__date">
+                - Septiembre 2013 - Marzo 2018 
+                Conectando github
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col__2">
+            <div className="service__box pointer">
+              <h1 className="service__text">
+              TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA
+              </h1>
+              <p className="compania service__text p__color">
+                  Ingeniería en Sistemas Computacionales 
+                </p>
+                <p className="p service__text p__color">
+                  -Organización y Funcionamiento de toda la estructura computacional.
+                </p>
+                <p className="p service__text p__color">
+                  -Me instruí conociendo los componentes principales en software y hardware.
+                </p>
+                <p className="p service__text p__color">
+                  - Analizar situaciones con el fin de conocer y solucionar complejidades en el sistema de cómputo.
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container">
         <div div className="row">
 
